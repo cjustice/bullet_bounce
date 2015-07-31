7a296539decd65ceeb00b1bb289cd901532a1f1f
jlashner <jlashner@gmail.com>
2015-07-31 17:19:48 -0400

    rotation

--- js/game.js
+++ js/game.js
@@ -1,86 +1,75 @@
-
-var game = new Phaser.Game(1200, 900, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(1200, 900, Phaser.CANVAS, 'bullet-bounce', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
-    game.load.tilemap('map', 'assets/tilemaps/maps/collision_test.json', null, Phaser.Tilemap.TILED_JSON);
-    game.load.image('ground_1x1', 'assets/tilemaps/tiles/ground_1x1.png');
-    game.load.image('walls_1x2', 'assets/tilemaps/tiles/walls_1x2.png');
-    game.load.image('tiles2', 'assets/tilemaps/tiles/tiles2.png');
-    game.load.image('ship', 'assets/sprites/thrust_ship2.png');
+	game.load.image('background','assets/tests/debug-grid-1920x1920.png');
+    game.load.image('player','assets/sprites/phaser-dude.png');
     game.load.image('bullet', 'assets/games/tanks/bullet.png');
 }
 
-var ship;
-var map;
-var layer;
+var player;
 var cursors;
+var bullets;
+var fireRate = 100;
+var nextFire = 0;
+var currentSpeed = 0;
+var elapsed = 0;
+var angle = 0;
 
 function create() {
-
-    game.physics.startSystem(Phaser.Physics.P2JS);
-
-    game.stage.backgroundColor = '#2d2d2d';
-
-    map = game.add.tilemap('map');
-
-    map.addTilesetImage('ground_1x1');
-    map.addTilesetImage('walls_1x2');
-    map.addTilesetImage('tiles2');
-    
-    layer = map.createLayer('Tile Layer 1');
-
-    layer.resizeWorld();
-
-    //  Set the tiles for collision.
-    //  Do this BEFORE generating the p2 bodies below.
-    map.setCollisionBetween(1, 12);
-
-    //  Convert the tilemap layer into bodies. Only tiles that collide (see above) are created.
-    //  This call returns an array of body objects which you can perform addition actions on if
-    //  required. There is also a parameter to control optimising the map build.
-    game.physics.p2.convertTilemap(map, layer);
-
-    ship = game.add.sprite(200, 200, 'ship');
-    game.physics.p2.enable(ship);
-
-    game.camera.follow(ship);
-
-    //  By default the ship will collide with the World bounds,
-    //  however because you have changed the size of the world (via layer.resizeWorld) to match the tilemap
-    //  you need to rebuild the physics world boundary as well. The following
-    //  line does that. The first 4 parameters control if you need a boundary on the left, right, top and bottom of your world.
-    //  The final parameter (false) controls if the boundary should use its own collision group or not. In this case we don't require
-    //  that, so it's set to false. But if you had custom collision groups set-up then you would need this set to true.
-    game.physics.p2.setBoundsToWorld(true, true, true, true, false);
-
-    //  Even after the world boundary is set-up you can still toggle if the ship collides or not with this:
-    // ship.body.collideWorldBounds = false;
-
+	game.world.setBounds(0, 0, 1920, 1920);
+	game.add.tileSprite(0, 0, 1920, 1920, 'background');
+    player = game.add.sprite(0, 0, 'player');
+    game.physics.enable(player, Phaser.Physics.ARCADE);
     cursors = game.input.keyboard.createCursorKeys();
     mouse = game.input.mousePointer;
+
+    player.anchor.set(.5);
 }
 
 function update() {
     console.log(game.time.elapsed);
-    ship.body.setZeroVelocity();
-    ship.body.setZeroRotation();
-    if (cursors.left.isDown) {
-        ship.body.moveLeft(300);
-    }
-    if (cursors.right.isDown) {
-        ship.body.moveRight(300);
-    }
-    if (cursors.up.isDown) {
-        ship.body.moveUp(300);
-    }
-    if (cursors.down.isDown) {
-        ship.body.moveDown(300);
-    }
 
-    mouse_pos
-    ship.body.angle = - (game.math.angleBetween(ship.body.x, ship.body.y, mouse.x, mouse.y) * 180/Math.PI - 180);
+    player.body.drag.set(0.2);
+    player.body.maxVelocity.setTo(400, 400);
+    player.body.collideWorldBounds = true;
+    player.bringToTop();
+    game.time.advancedTiming = true;
+    game.time.desiredFps = 30;
+}
+
+function update() {
+	//player.body.setZeroVelocity();
+	elapsed = game.time.elapsed;
+	//adjust = (elapsed/60);
+	//console.log(adjust);
+    if (cursors.left.isDown)
+    {
+        player.body.x -= (.4 * elapsed);
+    }
+    if (cursors.right.isDown)
+    {
+        player.body.x += (.4 * elapsed);
+    }
+    if (cursors.up.isDown)
+    {
+        //  The speed we'll travel at
+        player.body.y -= (.4 * elapsed);
+    }
+    if (cursors.down.isDown)
+    {
+        //  The speed we'll travel at
+        player.body.y += (.4 * elapsed);
+    }
+    
+    angle = -game.math.angleBetween(mouse.x,mouse.y, player.body.x, player.body.y) * 180/Math.PI;
+    // console.log(angle);
+    player.body.rotation = angle;
+    // console.log(player.body.rotation)    
+
+
+
 }
 
 function render() {
-
+	game.debug.text('FPS: ' + game.time.fps, 32, 32);
 }
