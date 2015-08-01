da3a0e30fac0a753b772040c24a8b3dcf34b4c0f
cjustice <connorjustice@gmail.com>
2015-08-01 14:28:55 -0400

    clients connect

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-//
 var express = require('express')
   , app = express(app)
   , server = require('http').createServer(app);
@@ -6,23 +5,66 @@ var express = require('express')
 // serve static files from the current directory
 app.use(express.static(__dirname));
 
+//we'll keep clients data here
+var clients = {};
+  
 //get EurecaServer class
-var EurecaServer = require('eureca.io').EurecaServer;
+var Eureca = require('eureca.io');
 
 //create an instance of EurecaServer
-var eurecaServer = new EurecaServer();
+var eurecaServer = new Eureca.Server({allow:['setId', 'spawnEnemy', 'kill']});
 
 //attach eureca.io to our http server
 eurecaServer.attach(server);
 
-//
+
+
+
+//eureca.io provides events to detect clients connect/disconnect
 
 //detect client connection
 eurecaServer.onConnect(function (conn) {    
     console.log('New Client id=%s ', conn.id, conn.remoteAddress);
+	
+	//the getClient method provide a proxy allowing us to call remote client functions
+    var remote = eurecaServer.getClient(conn.id);    
+	
+	//register the client
+	clients[conn.id] = {id:conn.id, remote:remote}
+	
+	//here we call setId (defined in the client side)
+	remote.setId(conn.id);	
 });
 
 //detect client disconnection
 eurecaServer.onDisconnect(function (conn) {    
     console.log('Client disconnected ', conn.id);
+	
+	var removeId = clients[conn.id].id;
+	
+	delete clients[conn.id];
+	
+	for (var c in clients)
+	{
+		var remote = clients[c].remote;
+		
+		//here we call kill() method defined in the client side
+		remote.kill(conn.id);
+	}	
 });
+
+
+eurecaServer.exports.handshake = function()
+{
+	//var conn = this.connection;
+	for (var c in clients)
+	{
+		var remote = clients[c].remote;
+		for (var cc in clients)
+		{		
+			remote.spawnEnemy(clients[cc].id, 0, 0);		
+		}
+	}
+}
+
+server.listen(8000);
