17aed12608f47172f964bf5eb91eba945092b5a0
jlashner <jlashner@gmail.com>
2015-08-01 14:52:07 -0400

    starting collision stuff

--- js/game.js
+++ js/game.js
@@ -18,7 +18,7 @@ var bullet_speed = 200;
 var nextFire = 0;
 var currentSpeed = 0;
 var angle = 0;
-var desired_movement = 100;
+var desired_movement = 30;
 var speed = 0;
 
 var wasd;
@@ -27,16 +27,19 @@ function create() {
     map = game.add.tilemap('map');
     map.addTilesetImage('testtiles_1x1','first_tiles_1x1')
 
+    map.setCollisionBetween(1, 12);
+
     layer = map.createLayer('Tile Layer 1');
     layer.resizeWorld();
+    game.add.existing(layer);
 
-    map.setCollisionBetween(1, 12);
+    
 
     //game.physics.convertTilemap(map, layer);
 
     //game.world.setBounds(0, 0, 1920, 1920);
     //game.add.tileSprite(0, 0, 1920, 1920, 'background');
-    player = game.add.sprite(0, 0, 'player');
+    player = game.add.sprite(100,100, 'player');
     game.physics.enable(player, Phaser.Physics.ARCADE);
     cursors = game.input.keyboard.createCursorKeys();
     mouse = game.input.mousePointer;
@@ -71,10 +74,12 @@ function update() {
 
 function render() {
     game.debug.text('FPS: ' + game.time.fps, 32, 32);
-    // game.debug.text('HERE);
+    
 }
 
 function move_player() {
+    game.physics.arcade.collide(player, layer);
+
     speed = (desired_movement / game.time.elapsed);
     if (wasd.left.isDown) {
         player.body.x -= speed
@@ -112,4 +117,7 @@ function shoot() {
 
 function render() {
     game.debug.text('FPS: ' + game.time.fps, 32, 32);
+
+    game.debug.bodyInfo(player, 32, 32);
+    game.debug.body(player);
 }
