ee6dadf64256acfc34b4e003aeebab78a93674d8
Alex <adrexler@wesleyan.edu>
2015-08-01 11:35:53 -0400

    match master

--- js/game.js
+++ js/game.js
@@ -1,7 +1,7 @@
 var game = new Phaser.Game(1200, 900, Phaser.CANVAS, 'bullet-bounce', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
-	game.load.image('background','assets/tests/debug-grid-1920x1920.png');
+    game.load.image('background','assets/tests/debug-grid-1920x1920.png');
     game.load.image('player','assets/sprites/blob-blue.png');
     game.load.image('bullet', 'assets/games/tanks/bullet.png');
 }
@@ -12,53 +12,57 @@ var bullets;
 var fireRate = 100;
 var nextFire = 0;
 var currentSpeed = 0;
-var elapsed = 0;
 var angle = 0;
+var desired_movement = 100;
+var speed = 0;
 
 function create() {
-	game.world.setBounds(0, 0, 1920, 1920);
-	game.add.tileSprite(0, 0, 1920, 1920, 'background');
+    game.world.setBounds(0, 0, 1920, 1920);
+    game.add.tileSprite(0, 0, 1920, 1920, 'background');
     player = game.add.sprite(0, 0, 'player');
     game.physics.enable(player, Phaser.Physics.ARCADE);
     cursors = game.input.keyboard.createCursorKeys();
     mouse = game.input.mousePointer;
-
     player.anchor.set(.5);
+
+    //adding player bullets
+    bullets = game.add.group();
+    bullets.enableBody = true;
+    bullets.physicsBodyType = Phaser.Physics.ARCADE;
+    bullets.createMultiple(30, 'bullet', 0, false);
+    bullets.setAll('anchor.x', 0.5);
+    bullets.setAll('anchor.y', 0.5);
+    bullets.setAll('outOfBoundsKill', true);
+    bullets.setAll('checkWorldBounds', true);
+    
+
+    //game.camera.follow(player);
 }
 
 function update() {
-    console.log(game.time.elapsed);
-
-    player.body.drag.set(0.2);
-    player.body.maxVelocity.setTo(400, 400);
-    player.body.collideWorldBounds = true;
-    player.bringToTop();
     game.time.advancedTiming = true;
-    game.time.desiredFps = 30;
+    angle = -game.math.angleBetween(mouse.x,mouse.y, player.body.x, player.body.y) * 180/Math.PI;
+    player.body.rotation = angle;
+    move_player();
 }
 
-function update() {
-	//player.body.setZeroVelocity();
-	elapsed = game.time.elapsed;
-	//adjust = (elapsed/60);
-	//console.log(adjust);
-    if (cursors.left.isDown)
-    {
-        player.body.x -= (.4 * elapsed);
+function render() {
+    game.debug.text('FPS: ' + game.time.fps, 32, 32);
+}
+
+function move_player() {
+    speed = (desired_movement / game.time.elapsed);
+    if (cursors.left.isDown) {
+        player.body.x -= speed
     }
-    if (cursors.right.isDown)
-    {
-        player.body.x += (.4 * elapsed);
+    if (cursors.right.isDown) {
+        player.body.x += speed;
     }
-    if (cursors.up.isDown)
-    {
-        //  The speed we'll travel at
-        player.body.y -= (.4 * elapsed);
+    if (cursors.up.isDown) {
+        player.body.y -= speed;
     }
-    if (cursors.down.isDown)
-    {
-        //  The speed we'll travel at
-        player.body.y += (.4 * elapsed);
+    if (cursors.down.isDown) {
+        player.body.y += speed;
     }
     
     angle = -game.math.angleBetween(mouse.x,mouse.y, player.body.x, player.body.y) * 180/Math.PI;
@@ -66,10 +70,17 @@ function update() {
     player.body.rotation = angle;
     // console.log(player.body.rotation)    
 
-
+    if (mouse.isDown) {
+        console.log ("shoot!");
+        shoot();
+    }
 
 }
 
+function shoot() {
+    
+}
+
 function render() {
-	game.debug.text('FPS: ' + game.time.fps, 32, 32);
+    game.debug.text('FPS: ' + game.time.fps, 32, 32);
 }
